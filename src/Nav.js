f3f5eb8b6e69da8677d42c52f1f6b91b44ea40de
leuneri <ericleung147258369@hotmail.com>
2023-05-10 21:02:21 -0800

    built nav bar

---
+++ src/Nav.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import './Nav.css'
+
+function Nav() {
+    const [show, handleShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
+            handleShow(true);
+        } else {
+            handleShow(false);
+        }
+    }
+
+    // As we scroll, we have an event listener for if we scrolled too much
+    useEffect(() => {
+        window.addEventListener('scroll', transitionNavBar);
+        return () => window.removeEventListener('scroll', transitionNavBar);
+    }, [])
+
+  return (
+    // Only have nav_black bar if show is true
+    <div className={`nav ${show && "nav_black"}`}>
+        <div className="nav_contents">
+            <img 
+                className='nav_logo'
+                src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                alt="Netflix Logo" />
+            <img 
+                className='nav_avatar' 
+                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" 
+                alt="Avatar" />
+        </div>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA7NtcemTArycRqoiIkdUnRCSfgQZUc2XI",
+    authDomain: "netflix-cl-9f7f0.firebaseapp.com",
+    projectId: "netflix-cl-9f7f0",
+    storageBucket: "netflix-cl-9f7f0.appspot.com",
+    messagingSenderId: "857987325156",
+    appId: "1:857987325156:web:655d7888b743f723b9a0f1",
+    measurementId: "G-Z3B0TR8656"
+  };
